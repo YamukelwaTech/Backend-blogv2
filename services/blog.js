7384cbd0f7c2bd64d17d2518565f49fe72eea742
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-06-11 04:00:36 +0200

    test13

--- services/blog.js
+++ services/blog.js
@@ -9,7 +9,7 @@ class Blog {
     this.pool = mysql.createPool({
       host: "127.0.0.1",
       user: "root",
-      password: process.env.DB_PASSWORD,
+      password: "mango22",
       database: "blog_db",
       waitForConnections: true,
       connectionLimit: 10,
