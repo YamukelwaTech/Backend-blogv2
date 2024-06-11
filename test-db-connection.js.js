fd24efe7c91fe8aa14f15c206c5249b72121c05d
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-06-11 08:02:56 +0200

    dummy data

--- test-db-connection.js.js
+++ test-db-connection.js.js
@@ -5,7 +5,7 @@ const testConnection = async () => {
   const pool = mysql.createPool({
     host: "127.0.0.1",
     user: "root",
-    password: process.env.DB_PASSWORD,
+    password: "mango22",
     database: "blog_db",
     waitForConnections: true,
     connectionLimit: 10,
