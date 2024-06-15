45452c8346a10025ad95b3f57a474bb4e4d01a21
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-06-15 16:59:59 +0200

    Chore: hope fix

--- services/blog.js
+++ services/blog.js
@@ -7,10 +7,10 @@ const fs = require("fs").promises;
 class Blog {
   constructor() {
     this.pool = mysql.createPool({
-      host: "localhost",
-      user: "root",
-      password: "mango22",
-      database: "blog_db",
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0,
